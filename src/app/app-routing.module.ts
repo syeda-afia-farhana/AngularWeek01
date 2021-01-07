b071787ddd0752efbae50e587e36476a4df80a72
syeda-afia-farhana <mouri.cse.suet@gmail.com>
2021-01-07 17:13:07 -0600

    Ongoing commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingComponent} from './pages/landing/landing.component';
+import {AboutComponent} from './pages/about/about.component';
+import {SupportComponent} from './pages/support/support.component';
+import {ResourcesComponent} from './pages/resources/resources.component';
+import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'landing', component: LandingComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'support', component: SupportComponent},
+  {path: 'resource', component: ResourcesComponent},
+  {path: '', redirectTo: '/landing', pathMatch: 'full' },
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SupportComponent } from './pages/support/support.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HeaderComponent } from './common/header/header.component';
+import { HtmlcssComponent } from './common/htmlcss/htmlcss.component';
+import { JavascriptComponent } from './common/javascript/javascript.component';
+import { AngularComponent } from './common/angular/angular.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutComponent,
+    SupportComponent,
+    ResourcesComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    HtmlcssComponent,
+    JavascriptComponent,
+    AngularComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
